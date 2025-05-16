ac6f130b05549c618a9d21221ccb4d8601db8e45
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-05-15 23:41:07 -0300

    feat(SplashScreen): implements splash screen with bootsplash library

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes/index'
-
+import BootSplash from "react-native-bootsplash";
 
 export default function App(){
   return(
-    <NavigationContainer>
+    <NavigationContainer  onReady={() => BootSplash.hide({fade: true})}>
       <StatusBar backgroundColor="#F0F4FF" barStyle="dark-content"/>
       <Routes />
     </NavigationContainer>
